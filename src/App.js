8db7eb54f26336f64fbe2e449655d56105a7993b
Rafet Abdalgalil <rafetalani@gmail.com>
2022-08-05 10:13:47 -0700

    added the auth reducer and action creaters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/NavBar/Navbar";
 const App = () => {
     return (
         <div>
-            <h1>Hello World!!</h1>
             <Navbar />
             <AllRoutes />
         </div>
---
+++ src/store/auth.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+
+// Action Types
+const SET_AUTH = 'SET_AUTH';
+const SET_AUTH_ERROR = 'SET_AUTH_ERROR';
+const TOKEN = 'token';
+
+// Action Creators
+export const setAuth = user => ({
+    type: SET_AUTH,
+    user,
+})
+
+export const setError = error => ({
+    type: SET_AUTH_ERROR,
+    error,
+})
+
+// Thunk Creators
+
+export const userInfo = () => async dispatch => {
+    // get the token from the local storage
+    const token = window.localStorage.getItem(TOKEN);
+    if (token) {
+        // if the token is valid, get the user info
+        const { data: user } = await axios.get('/auth/me', {
+            headers: {
+                authorization: token,
+            }
+        })
+        // set the user info in the store
+        dispatch(setAuth(user));
+    }
+}
+
+export const authennticate = (username, password, method) => async dispatch => {
+    try {
+        // get the token from the server
+        const { data } = await axios.post(`auth/${method}`, { username, password});
+        // set the token in the local storage
+        window.localStorage.setItem(TOKEN, data.token);
+        // invoke the user info action creator to get the user info
+        dispatch(userInfo());
+    } catch (authError) {
+        // set the error in the store
+        return dispatch(setError({ error: authError }));
+    }
+}
+
+export const logout = () => dispatch => {
+    // remove the token from the local storage
+    window.localStorage.removeItem(TOKEN);
+    // remove the user info from the store
+    dispatch(setAuth({}));
+    // redirect the user to the login page
+    history.push('/');
+}
+
+export const clearError = () => dispatch => {
+    dispatch(setError({}));
+}
+
+// Reducer
+const initialState = {
+    user: {},
+    error: {}
+}
+
+export const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_AUTH:
+            // return a new state with the user info
+            return { ...state, user: action.user };
+        case SET_AUTH_ERROR:
+            // return a new state with the error
+            return { ...state, error: action.error};
+        default:
+            return state;
+    }
+}
+
